87a22c381c546f8dbc4c8d917d30faa0df1d929b
Crobat4 <104547700+Crobat4@users.noreply.github.com>
2022-06-26 16:39:23 -0300

    [Crobat v.0.2.5]

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -2,6 +2,7 @@ import './temporaryWindowInjection';
 
 // Ensure that the Knockout Extenders are injected
 import './koExtenders';
+/*
 import { addListener, launch } from 'devtools-detector';
 // const view = document.createElement('div');
 // document.body.appendChild(view);
@@ -10,13 +11,14 @@ import { addListener, launch } from 'devtools-detector';
 // eslint-disable-next-line no-unused-vars
 addListener(
     () => (
-    /*
-    (view.innerText = isOpen
-    ? 'devtools status: open'
-    : 'devtools status: close')
-    */
+    
+    //(view.innerText = isOpen
+    //? 'devtools status: open'
+    //: 'devtools status: close')
+    
         window.location.replace('assets/images/crobatgun.png')
     ),
 );
 // 2. launch detect
 launch();
+*/
